4bd375cbc1b3aad4b11194fcca5f174392caff9c
Mujassim Sheikh <mujasseim@gmail.com>
2017-09-12 14:37:58 +0200

    part 3 solution commit

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import {Http, Response} from '@angular/http'
+import {Http, Response} from '@angular/http';
+import {HttpParams} from '@angular/common/http';
 import 'rxjs/add/operator/map';
 import {Observable} from 'rxjs';
 
@@ -17,9 +18,10 @@ export class AppComponentService {
    }
 
     getAlbumDetails(userId:number):Observable<Array<any>>{ 
-       return this.http.request('http://jsonplaceholder.typicode.com/users/1/albums/').map((res:Response) => res.json());
+       return this.http.request(`http://jsonplaceholder.typicode.com/users/${userId}/albums`)
+       .map(this.responseData);
    }
    getPhotoDetails(albumId:number):Observable<Array<any>>{ 
-       return this.http.request('http://jsonplaceholder.typicode.com/albums/1/photos/').map((res:Response) => res.json()) ;
+       return this.http.request(`http://jsonplaceholder.typicode.com/albums/${albumId}/photos/`).map((res:Response) => res.json()) ;
    }
 }
