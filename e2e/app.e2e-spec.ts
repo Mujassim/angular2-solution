6c0a4b5b98842cb1abd9954990bdb168a2843174
Mujassim Sheikh <mujasseim@gmail.com>
2017-09-13 00:02:22 +0200

    final commit

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,64 @@
-import { APMPage } from './app.po';
+import { UsersDisplayPage } from './app.po';
+import { browser, element, by} from 'protractor';  
 
-describe('apm App', () => {
-  let page: APMPage;
+describe('users Details App', () => {
+  let page: UsersDisplayPage;
 
   beforeEach(() => {
-    page = new APMPage();
+    page = new UsersDisplayPage();
   });
 
-  it('should display welcome message', () => {
+  it('Welcome page should display the below header', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to Angular: Getting Started!!');
+    expect(page.getHeadingText()).toEqual('User Details');
   });
+
+  it('should display a list of users', () => {
+    page.navigateTo();
+    expect(page.getUserDetails().count()).toBeGreaterThan(1);
+  });
+});
+
+describe('Albums Details App', () => {
+    let page: UsersDisplayPage;
+
+  beforeEach(() => {
+    page = new UsersDisplayPage();
+  });
+
+  it("should be able to click on a user Name on the user details page and go to the album details page after a http service call", () => {  
+    page.navigateTo();
+    let id=page.getFirstUserId().getText();
+    page.getFirstUser().click();
+    expect(page.getAlbumDetailsFromUserId().count()).toBeGreaterThan(1);
+})
+});
+
+describe('Photos Details App', () => {
+    let page: UsersDisplayPage;
+
+  beforeEach(() => {
+    page = new UsersDisplayPage();
+  });
+
+  it("should be able to click on a title on the album details page and go to the photos page after a http service call", () => {  
+    let id=page.getFirstAlbumId().getText();
+    page.getFirstAlbum().click();
+    expect(page.getphotosFromAlbumId().count()).toBeGreaterThan(1);
+})
+});
+
+describe('Exit button', () => {
+    let page: UsersDisplayPage;
+
+  beforeEach(() => {
+    page = new UsersDisplayPage();
+  });
+
+  it("should go to the users details page", () => {  
+    page.clickOnExit();
+    browser.getCurrentUrl().then(function(actualUrl){ // promise
+            expect(actualUrl.indexOf('users') !== -1).toBeTruthy(); // check the current url is list
+          });
+})
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,11 +1,43 @@
 import { browser, by, element } from 'protractor';
 
-export class APMPage {
+export class UsersDisplayPage {
   navigateTo() {
     return browser.get('/');
   }
-
-  getParagraphText() {
-    return element(by.css('pm-root h1')).getText();
+  getHeadingText() {
+    // Get the home page heading element reference
+    return element(by.css('user-list h3')).getText();
   }
+  getUserDetails() {
+    return element.all(by.css('.userId-class'));
+  }
+
+  getFirstUser() {
+    return element(by.css('.username-class'));
+  }
+
+   getFirstUserId() {
+    return element(by.css('.userId-class'));
+  }
+
+   getAlbumDetailsFromUserId() {
+    return element.all(by.css('.albumId-class'));
+  }
+
+  getFirstAlbum() {
+    return element(by.css('.albumname-class'));
+  }
+
+   getFirstAlbumId() {
+    return element(by.css('.albumId-class'));
+  }
+
+   getphotosFromAlbumId() {
+    return element.all(by.css('.photos-class'));
+  }
+
+clickOnExit()
+{
+  return element(by.css('.click-class')).click();
+}
 }
